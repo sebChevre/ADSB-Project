24cdedd891995eb21e98ae577537978d3725151f
Sébastien Chèvre <seb.chevre@gmail.com>
2019-11-28 07:45:08 +0100

    structure

--- adsbservice/src/main/java/ch/abbaye11/adsbservice/SpringIntegrationMessageAppPublishSubscribe.java
+++ adsbservice/src/main/java/ch/abbaye11/adsbservice/SpringIntegrationMessageAppPublishSubscribe.java
@@ -4,13 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.http.config.EnableIntegrationGraphController;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @Slf4j
 @EnableScheduling
-@EnableIntegration
+@EnableIntegrationGraphController
+
 public class SpringIntegrationMessageAppPublishSubscribe  {
 
     public static void main(String[] args) {
